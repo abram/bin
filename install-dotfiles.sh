230e4a77651d729429b751e3fe9625660d025542
Abe Fettig <abe@fettig.net>
2013-09-13 22:21:55 -0400

    Fix bugs in install-dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,8 +3,8 @@ cd ~/dotfiles
 for file in *; do
     if [ -e ~/.$file ]; then
 	echo "Moving existing .$file to $.file.old"
-	mv ~/.$file ~/.$file.old
+	mv ~/.$file ~/.${file}.old
     fi
     echo "Creating symlink to .$file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s ~/dotfiles/$file ~/.$file
 done
